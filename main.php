a2c49afe45d8acfab338700b8655f691eb5d79df
Aleksey Yefremenko <alexeyefr@gmail.com>
2023-07-22 15:34:40 +0300

    Update: improve main.php

--- main.php
+++ main.php
@@ -17,10 +17,13 @@ function getTransportItems(): array
     $result = [];
     $autoDriver = new AutoDriver();
     $busDriver = new BusDriver();
+    $experiencedBusDriver = new BusDriver(8.0);
     $minibusDriver = new MinibusDriver();
     $motorbikeDriver = new MotorbikeDriver();
 
     $result[] = new Bus($busDriver);
+    $result[] = new Bus($experiencedBusDriver, 'Bus with experienced driver');
+    $result[] = new Bus($busDriver, 'Bus on cheaper fuel', 35.0);
     $result[] = new Auto($autoDriver);
     $result[] = new Minibus($minibusDriver);
     $result[] = new Motorbike($motorbikeDriver);
@@ -34,6 +37,15 @@ function calculateTravelCost(int $passengers, int $baggage, int $distance): void
      * @var BaseTransport[] $transportItems
      */
     $transportItems = getTransportItems();
+
+    echo '---------- Begin ----------' . "\n";
+    echo 'Request: ' . "\n";
+    echo 'Passengers: ' . $passengers . "\n";
+    echo 'Baggage: ' . $baggage . "\n";
+    echo 'Distance: ' . $distance . "\n";
+    echo "\n";
+    echo 'Response: ' . "\n";
+    echo "\n";
 
     foreach ($transportItems as $transportItem) {
         $ability = $transportItem->checkAbility($passengers, $baggage, $distance);
@@ -49,10 +61,13 @@ function calculateTravelCost(int $passengers, int $baggage, int $distance): void
 
         echo "\n";
     }
+
+    echo '---------- End ----------' . "\n";
+    echo "\n";
 }
 
 $passengers = 4;
 $baggage = 60;
-$distance = 250;
+$distance = 175;
 
 calculateTravelCost($passengers, $baggage, $distance);
